b2c91c85fb745d75f29fc4b46fc87acb7ed67b9d
ZameerNoori <zameer.noori3@gmail.com>
2022-06-12 15:31:21 -0400

    Class15

---
+++ src/com/syntax/class15/VariablesDemo2.java
@@ -0,0 +1,15 @@
+package com.syntax.class15;
+
+public class VariablesDemo2 {
+    int num = 10;// whenever you have to share some info in more than one method of a class use instance variables
+
+    void method1() {
+        int num2 = 20;
+        System.out.println(num);
+    }
+
+    void method2() {
+        //  System.out.println(num2);
+        System.out.println(num);
+    }
+}
